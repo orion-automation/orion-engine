00807083c9abb09e5301a14d51d03eda9f5d9c67
jaxhuan <jax@eorionsolution.com>
2025-02-27 10:33:22 +0800

    fix :编译错误修复

--- src/main/frontend/src/components/common/DMNModeler.vue
+++ src/main/frontend/src/components/common/DMNModeler.vue
@@ -81,7 +81,7 @@ export default {
             DmnPropertiesPanelModule,
             DmnPropertiesProviderModule,
             CamundaPropertiesProviderModule
-          ]
+          ],
           keyboard: {
             bindTo: window
           },
